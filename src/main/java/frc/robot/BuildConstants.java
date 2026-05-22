8bc53236a4e1ba5df824c843953984f708364c6b
bagleycelia-ctrl <bagleycelia@gmail.com>
2026-05-22 11:45:33 -0400

    fixed gradle and made it build

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "2026-CeliaBagley-PerformanceTask";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 2;
+    public static final String GIT_SHA = "0a26449a1e7d22c6c18c364fdb101641a23cf6ca";
+    public static final String GIT_DATE = "2026-05-22 08:57:15 EDT";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2026-05-22 10:21:11 EDT";
+    public static final long BUILD_UNIX_TIME = 1779459671794L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants(){}
+}
