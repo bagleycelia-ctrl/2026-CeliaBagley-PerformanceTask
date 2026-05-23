ba380f768a475797a3f5cd31ea9a7216bdc0026d
bagleycelia-ctrl <bagleycelia@gmail.com>
2026-05-22 23:38:24 -0400

    added motion magic

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOKraken.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOKraken.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.elevator;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class ElevatorIOKraken implements ElevatorIO {
@@ -10,13 +13,33 @@ public class ElevatorIOKraken implements ElevatorIO {
     private final TalonFX rightMotor =
             new TalonFX(ElevatorConstants.RIGHT_MOTOR_ID);
 
+    private final MotionMagicVoltage motionMagicRequest =
+            new MotionMagicVoltage(0);
+
+    public ElevatorIOKraken() {
+
+        TalonFXConfiguration config =
+                new TalonFXConfiguration();
+
+        config.Slot0.kP = ElevatorConstants.kP;
+        config.Slot0.kI = ElevatorConstants.kI;
+        config.Slot0.kD = ElevatorConstants.kD;
+
+        config.MotionMagic.MotionMagicCruiseVelocity = 3.0;
+        config.MotionMagic.MotionMagicAcceleration = 6.0;
+    }
+
     @Override
     public void updateInputs(ElevatorIOInputs inputs) {
+
         inputs.positionMeters =
                 leftMotor.getPosition().getValueAsDouble();
 
         inputs.velocityMetersPerSecond =
                 leftMotor.getVelocity().getValueAsDouble();
+
+        inputs.appliedVolts =
+                leftMotor.getMotorVoltage().getValueAsDouble();
 
         inputs.currentAmps =
                 leftMotor.getSupplyCurrent().getValueAsDouble();
@@ -24,18 +47,17 @@ public class ElevatorIOKraken implements ElevatorIO {
 
     @Override
     public void setPosition(double meters) {
-        // Motion Magic request would go here
+
+        leftMotor.setControl(
+                motionMagicRequest.withPosition(meters));
     }
 
     @Override
     public void setVoltage(double volts) {
         leftMotor.setVoltage(volts);
-        rightMotor.setVoltage(volts);
     }
 
-    @Override
     public void stop() {
         leftMotor.stopMotor();
-        rightMotor.stopMotor();
     }
 }
